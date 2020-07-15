2037f7bd311e0b1cc736cf2000a6642960f34242
DivadNojnarg <dgranjon@ymail.com>
2020-07-15 16:10:40 +0200

    add progress bars

--- R/components.R
+++ R/components.R
@@ -32,3 +32,70 @@ jumbotron_386 <- function(..., title = NULL) {
 }
 
 
+
+
+#' Create a Bootstrap 386 progress bar
+#'
+#' The progress bar may be updated server side. See \link{update_progress_386}.
+#'
+#' @param id Progress unique id.
+#' @param value Progress value. Numeric between 0 and 100.
+#' @param status Progress status.
+#'
+#' @return A progress bar tag.
+#' @export
+#' @seealso \link{update_progress_386}
+progress_386 <- function(id = NULL, value, status = NULL) {
+
+  validate_progress_value(value)
+  if (!is.null(status)) validate_status(status)
+  bar_cl <- paste0("progress-bar", if(!is.null(status)) paste0(" bg-", status))
+
+  div(
+    class = "progress",
+    div(
+      id = id,
+      class = bar_cl,
+      style = paste0("width: ", value, "%"),
+      role = "progressbar",
+      `aria-valuenow` = as.character(value),
+      `aria-valuemin` = "0",
+      `aria-valuemax` = "100",
+      span(class = "sr-only", paste0(value,"% complete"))
+    )
+  )
+}
+
+
+
+#' Update a \link{progress_386} on the client
+#'
+#' @param id Progress unique id.
+#' @param value New value.
+#' @param session Shiny session object.
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(shiny)
+#'  library(shiny386)
+#'  ui <- page_386(
+#'   button_386("update", "Update"),
+#'   br(),
+#'   progress_386(id = "progress1", 12)
+#'  )
+#'
+#'  server <- function(input, output, session) {
+#'    observeEvent(input$update, {
+#'      update_progress_386(
+#'        id = "progress1",
+#'        sample(1:100, 1)
+#'      )
+#'    })
+#'  }
+#'  shinyApp(ui, server)
+#' }
+update_progress_386 <- function(id, value, session = shiny::getDefaultReactiveDomain()) {
+  message <- list(id = session$ns(id), value = value)
+  session$sendCustomMessage(type = "update-progress", message)
+}
--- R/deps.R
+++ R/deps.R
@@ -3,7 +3,11 @@ bs4_deps <- htmltools::htmlDependency(
   version = "4.4.1",
   src = c(file = "bootstrap-4.4.1"),
   package = "shiny386",
-  script = c("js/bootstrap.bundle.js", "js/custom.js"),
+  script = c(
+    "js/bootstrap.bundle.js",
+    "js/custom.js",
+    "js/progress_handler.js"
+  ),
   stylesheet = "css/bootstrap.min.css"
 )
 
