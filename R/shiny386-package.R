89f37f3823945bf9eb7a3d0cc41ca0c1a3cd4b01
DivadNojnarg <dgranjon@ymail.com>
2020-07-22 01:24:48 +0200

    Initial gh-pages commit

--- R/shiny386-package.R
+++
@@ -1,9 +0,0 @@
-#' shiny386
-#'
-#' Old school Bootstrap 4 template for Shiny
-#'
-#' @name shiny386
-#' @import htmltools
-#' @import shiny
-#' @docType package
-NULL
--- inst/bootstrap-4.4.1/js/progress_handler.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  Shiny.addCustomMessageHandler('update-progress', function(message) {
-    $('#' + message.id).css('width', message.value +'%');
-  });
-});
